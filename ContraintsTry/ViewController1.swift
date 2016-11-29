c171db474593dc4f9a1e84f26b43fb8ac358b27e
Amrit <tiwariammit@gmail.com>
2016-11-29 12:20:48 +0545

    files add

--- ContraintsTry/ViewController1.swift
+++ ContraintsTry/ViewController1.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ViewController1.swift
 //  ContraintsTry
 //
 //  Created by Amrit on 11/29/16.
@@ -8,29 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController1: UIViewController {
+    
     @IBOutlet weak var stackView: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
         if UIScreen.main.bounds.height < UIScreen.main.bounds.width {
             self.stackView.axis = .horizontal
         }
-  
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    override func willRotate(to toInterfaceOrientation: UIInterfaceOrientation, duration: TimeInterval) {
-        if (toInterfaceOrientation.isLandscape) {
-            self.stackView.axis = .horizontal
-        }
-        else {
-            self.stackView.axis = .vertical
-        }
-    }
-
+//    override func willRotate(to toInterfaceOrientation: UIInterfaceOrientation, duration: TimeInterval) {
+//        if (toInterfaceOrientation.isLandscape) {
+//            self.stackView.axis = .horizontal
+//        }
+//        else {
+//            self.stackView.axis = .vertical
+//        }
+//    }
+    
 }
-
